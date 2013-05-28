59db7749adb51d4dabfb9c0401d2dd98c2e311cb
sergio ledezma vazquez <sergio.rafael.ledezma.vazquez@gmail.com>
2013-05-28 11:54:21 -0500

    probando git

---
+++ src/tiendasears/PruebaGIT.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendasears;
+
+/**
+ *
+ * @author sergio
+ */
+public class PruebaGIT {
+    
+}
